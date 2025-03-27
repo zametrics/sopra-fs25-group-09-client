71f9a2f2c85c75e8aee1a5b2346e43af6b43ed04
zametrics <88628200+zametrics@users.noreply.github.com>
2025-03-27 19:18:17 +0100

    Update domain.ts

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -7,7 +7,7 @@ import { isProduction } from "@/utils/environment";
  */
 export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    "sopra-fs25-group-09-server.oa.r.appspot.com"; // TODO: update with your production URL as needed.
+    "https://sopra-fs25-group-09-server.oa.r.appspot.com"; // TODO: update with your production URL as needed.
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 }
